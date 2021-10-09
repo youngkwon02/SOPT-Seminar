338a76bc53a8a63c108d88178057d849ba33b7cf
youngkwon02 <youngkwon02@naver.com>
2021-10-09 16:32:25 +0900

    refactor: calculator module

---
+++ seminar/seminar-02/calculator-main.js
@@ -0,0 +1,6 @@
+const calculator = require('./calculator');
+
+console.log(calculator.sum(1, 2));
+console.log(calculator.subtract(1, 2));
+console.log(calculator.multiply(1, 2));
+console.log(calculator.divide(1, 2));
---
+++ seminar/seminar-02/calculator.js
@@ -0,0 +1,13 @@
+const sum = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+const calculator = {
+  sum,
+  subtract,
+  multiply,
+  divide
+}
+
+module.exports = calculator;
