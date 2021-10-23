8ee4e9dd143de86cc881b6650bf84a49201de041
youngkwon02 <youngkwon02@naver.com>
2021-10-23 16:30:52 +0900

    feat: /user/login logic of my own

--- seminar/seminar-03/crud/routes/user.js
+++ seminar/seminar-03/crud/routes/user.js
@@ -5,6 +5,7 @@ const statusCode = require("../constant/statusCode");
 const responseMessage = require("../constant/responseMessage");
 
 const users = require("../dbmockup/users");
+const { fail, success } = require("../lib/util");
 
 //    /user/signup
 router.post("/signup", (req, res) => {
@@ -37,6 +38,26 @@ router.post("/signup", (req, res) => {
   };
 
   res.status(200).send(newUser);
+});
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res
+      .status(statusCode.BAD_REQUEST)
+      .send(fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
+  }
+  const emailMatched = users.filter((o) => o.email === email);
+  if (emailMatched.length === 0) {
+    return res
+      .status(statusCode.BAD_REQUEST)
+      .send(fail(statusCode.BAD_REQUEST, responseMessage.NO_USER));
+  }
+  const passwordMatched = emailMatched.filter(o => o.password === password);
+  if (passwordMatched.length === 0) {
+    return res.status(statusCode.BAD_REQUEST).send(fail(statusCode.BAD_REQUEST, responseMessage.MISS_MATCH_PW));
+  }
+  res.status(statusCode.OK).send(success(statusCode.OK, responseMessage.LOGIN_SUCCESS, {id: passwordMatched.id, email: passwordMatched.email, name: passwordMatched.name}));
 });
 
 module.exports = router;
